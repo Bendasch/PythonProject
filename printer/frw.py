1883da90de506df5a3e850ee902ed50448d2dfa1
Bendasch <benediktscherner@posteo.de>
2018-02-24 17:19:41 +0100

    Änderung am FRW
    Parameter 'data' ist jetzt Liste. Die Syntax, um dem Constructor die Parameter damit zu liefern ist schlicht "instance = classobj(*data)".
    
    Anpassung der Printerklasse -> zweites Attribut und entsprechende Methoden, um die Anpassung von frw zu testen. Zusätzlich "attributes" Attribut, das eine Liste aus zwei Listen enthält, die die Attributnamen der Klasse sowie deren Datentyp enthält.

--- printer/frw.py
+++ printer/frw.py
@@ -1,5 +1,5 @@
 def create_instance( modname, classname, data ):
 	module = __import__(modname)
 	classobj = getattr(module, classname)
-	instance = classobj(data)
+	instance = classobj(*data)
 	return instance
